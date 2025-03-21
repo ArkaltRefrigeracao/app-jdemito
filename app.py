cdf76f697e967e06e5c9883ceaffc49a698557d6
ArkaltRefrigeracao <supervisor@arkalt.com.br>
2025-03-21 16:33:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ for idx, (peca, codigo) in enumerate(pecas_disponiveis):
     unique_key = f"checkbox_{idx}"
     if st.checkbox(f"{peca} (Código: {codigo})", key=unique_key):
         if codigo == 6196:
-            link_imagem = "https://drive.google.com/uc?export=view&id=1FrYBRZxQGq3TYHT39f1ccw7cEg8G_tBq"
+            link_imagem = "/mnt/data/image.png"
         else:
             link_imagem = ""
         pecas_selecionadas.append((peca, codigo, link_imagem))
