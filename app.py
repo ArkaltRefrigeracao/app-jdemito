99d42059dd21178f78b254ff2b312d59ed0d43e8
ArkaltRefrigeracao <supervisor@arkalt.com.br>
2025-03-21 14:40:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ df_placas, df_pecas = load_data()
 col1, col2, col3 = st.columns([1, 3, 1])  
 
 with col1:
-    st.image("arkaltfoto.jpg", width=120)  
+    st.image("arkaltfoto.JPG", width=120)  
 
 with col2:
     st.markdown("<h1 style='text-align: center; color: orange;'>CATÁLOGO DE PEÇAS</h1>", unsafe_allow_html=True)
